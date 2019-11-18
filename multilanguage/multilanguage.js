f28dab6d2d7bd6e978a664d901dd25646de36483
Vincent Honca <vincenth1985@gmail.com>
2019-11-18 20:39:56 +0100

    reformat projet

--- multilanguage/multilanguage.js
+++ multilanguage/multilanguage.js
@@ -1,17 +1,69 @@
 var language = {
     eng: {
-        reservez: "Order Now"
+        reservez: "Order Now",
+
+        services: "PRIVATE DRIVER SERVICE",
+
+        servicesExplication: "You are visiting Belgium but you do not want to waste time with taxi services or\n" +
+            "            public transport? Mn-Limousine offers a tailor-made service. You will be taken care of by a\n" +
+            "            experienced private driver all over Belgium.",
+
+
+        vehiculeComfortable: "COMFORTABLE AND LUXURY VEHICLE",
+
+        vehiculeComfortableExplication: "An experienced driver with a luxury car or minibus.\n" +
+            "            VIP cars with drivers for the frames",
+
+
+        voyagerSerainement :"TRAVEL SERABLY",
+
+        voyagerSerainementExplication : "No waste of time, no worry for the most efficient route, the greatest comfort.",
+
+        transfert : "AIRPORT TRANSFER",
+
+        transfertexplication:"For those who do not wish to decipher foreign public transport cards or negotiate with\n" +
+            "            local taxi companies, Mn-limousine offers a service that will take you directly to your\n" +
+            "            airport.",
+
+
     },
+
     fr: {
+
         reservez: "Reservez maintenant"
+
     }
+
 };
 
 //define language via window hash
 if (window.location.hash) {
 
     if (window.location.hash === "#eng") {
+
         res.textContent = language.eng.reservez;
+
+        ser.textContent = language.eng.services;
+        serex.textContent = language.eng.servicesExplication;
+
+        veh.textContent = language.eng.vehiculeComfortable;
+        vehex.textContent = language.eng.vehiculeComfortableExplication;
+
+        voy.textContent= language.eng.voyagerSerainement;
+        voyex.textContent=language.eng.voyagerSerainementExplication;
+
+        tra.textContent=language.eng.transfert;
+        traex.textContent=language.eng.transfertexplication;
+
+
+
+
+
+
+
+
+
+
     }
 
     if (window.location.hash === "#fr") {
@@ -20,6 +72,6 @@ if (window.location.hash) {
 }
 
 function reload(hash) {
-    location.assign(location.replace(hash))
-    location.reload()
+    location.assign(location.replace(hash));
+    location.reload();
 }
